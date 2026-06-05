8f81222d0d32b81459bf060a8c3c40c559d749dc
mahdi Developer <mahdi.monavari1381@gmail.com>
2026-06-05 09:28:24 +0330

    title header coding

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,5 +1,6 @@
 import Abilities from "@/components/templates/index/abilities/Abilities"
 import Hero from "@/components/templates/index/hero/Hero"
+import ReservationRoadMap from "@/components/templates/index/reservationRoadMap/ReservationRoadMap"
 async function page() {
   return (
     <>
@@ -11,6 +12,7 @@ async function page() {
     <div className="space-y-20 bg-zinc-100 dark:bg-zinc-800 dark:text-white mb-1000 pt-20">
       <div className="container">
         <Abilities/>
+        <ReservationRoadMap/>
       </div>        
     </div>    
     </>
--- src/components/templates/index/abilities/Abilities.js
+++ src/components/templates/index/abilities/Abilities.js
@@ -5,33 +5,26 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper/modules';
+import TitleHeader from '@/components/modules/titleHeader/TitleHeader';
 
 export default function Abilities() {
   return (
-    <>
-      <h2 className='mb-5 text-3xl text-emerald-900 font-Morabba-Bold'>خدمات برتری که ارائه می‌دهیم</h2>
-      
+    <>      
+    <TitleHeader title="خدمات برتری که ارائه میدهیم"/>      
       <div className="w-full py-10">
         <Swiper
-          // --- تنظیمات اصلی ---
           spaceBetween={30}    
-          allowTouchMove={true} // قابلیت ورق زدن فعال
+          allowTouchMove={true}
           slidesPerView={2}
-          loop={true}           // ✅ قابلیت برگشت از آخر به اول (بی‌نهایت)
-          
-          // --- تنظیمات Pagination ---
+          loop={true}
           pagination={{
-            clickable: true,    // پیشنهاد: اگر می‌خواهید کاربر با کلیک روی نقاط هم جابه‌جا شود true بگذارید
-          }}
-          
-          // --- تنظیمات Autoplay ---
+            clickable: true,
+          }}        
           modules={[Pagination, Autoplay]}
           autoplay={{
             delay: 5000, 
-            disableOnInteraction: false, // اگر کاربر ورق زد، خودکار متوقف نشود
+            disableOnInteraction: false,
           }}
-          
-          // --- تنظیمات ریسپانسیو ---
           breakpoints={{
             320: {
               slidesPerView: 1,
@@ -44,10 +37,8 @@ export default function Abilities() {
             
           }}
           
-          // اضافه کردن padding برای نمایش بهتر نقاط پایین اسلایدر
           className="mySwiper"
         >    
-          {/* ❌ دیو با کلاس grid را حذف کردیم. اسلایدها باید مستقیم اینجا باشند */}
           {offerServices.map((offer) => (
             <SwiperSlide key={offer.id}>
               <Abilite {...offer} />

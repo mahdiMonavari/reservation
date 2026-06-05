c13f36cbcdec5ab863ee709e2b4ceac604e5edb8
mahdi Developer <mahdi.monavari1381@gmail.com>
2026-06-05 19:28:49 +0330

    create roadmap code

--- src/components/modules/navbar/navbar.js
+++ src/components/modules/navbar/navbar.js
@@ -5,14 +5,14 @@ function Navbar({theme}) {
   return (
     <div className="fixed top-5 px-8 py-4 bg-green-100/50 dark:bg-teal-800/70 hidden backdrop-blur-sm
      rounded-full sm:w-154 md:w-186 lg:w-250 xl:w-314 w-[90%]  left-1/2 -translate-x-1/2 sm:flex border-l-2 border-green-300 border-r-2
-    items-center justify-between z-50 overflow-hidden">
+    items-center justify-between z-50 overflow-hidden dark:text-gray-100">
       {/* <span className='w-25 h-px bg-green-300 absolute top-0 right-3 rounded-3xl'></span> */}
     <ul className="flex items-center gap-x-6 text-green-800/80 font-Morabba-Bold  *:cursor-pointer text-xl">
-        <li className='text-green-800/80 hover:text-green-800 transition-all dark:text-teal-950 dark:hover:text-teal-500 duration-300'>خانه</li>
-        <li className='text-green-800/80 hover:text-green-800 transition-all dark:text-teal-950 dark:hover:text-teal-500 duration-300'>درباره ما</li>
-        <li className='text-green-800/80 hover:text-green-800 transition-all dark:text-teal-950 dark:hover:text-teal-500 duration-300'>ارتباط با ما</li>
-        <li className='text-green-800/80 hover:text-green-800 transition-all dark:text-teal-950 dark:hover:text-teal-500 duration-300'>دکترها</li>
-        <li className='text-green-800/80 hover:text-green-800 transition-all dark:text-teal-950 dark:hover:text-teal-500 duration-300'>رزرو نوبت</li>
+        <li className='text-green-800/80  hover:text-green-800 transition-all dark:text-gray-100 dark:hover:text-teal-500 duration-300'>خانه</li>
+        <li className='text-green-800/80  hover:text-green-800 transition-all dark:text-gray-100 dark:hover:text-teal-500 duration-300'>درباره ما</li>
+        <li className='text-green-800/80  hover:text-green-800 transition-all dark:text-gray-100 dark:hover:text-teal-500 duration-300'>ارتباط با ما</li>
+        <li className='text-green-800/80  hover:text-green-800 transition-all dark:text-gray-100 dark:hover:text-teal-500 duration-300'>دکترها</li>
+        <li className='text-green-800/80  hover:text-green-800 transition-all dark:text-gray-100 dark:hover:text-teal-500 duration-300'>رزرو نوبت</li>
     </ul>
      <ul className="flex items-center gap-x-4 text-green-800/80 font-Morabba-Bold  *:cursor-pointer text-xl
      dark:text-teal-950">        

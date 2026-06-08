ba8572f09e46b2456dcacbf32b8aa17dd2a9e78e
mahdi Developer <mahdi.monavari1381@gmail.com>
2026-06-08 10:37:31 +0330

    create db connection

---
+++ src/utiles/DB/connection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+const connectionToDB=async()=>{
+    try{
+        if(mongoose.connections[0].readyState){
+            return true
+        }
+        await mongoose.connect("mongodb://127.0.0.1:27017/next-db");
+        console.log("DB connected");
+    }catch(err){
+        console.log("has error for DB connection");
+    }
+}

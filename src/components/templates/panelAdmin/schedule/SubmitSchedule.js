8a951480288a0221ffb229ae426b0b94b933f7f7
mahdi Developer <mahdi.monavari1381@gmail.com>
2026-07-04 10:28:47 +0330

    handle reason of start and end hour in working date

--- src/components/templates/panelAdmin/schedule/SubmitSchedule.js
+++ src/components/templates/panelAdmin/schedule/SubmitSchedule.js
@@ -1,10 +1,25 @@
-import { successToast } from "@/components/modules/toast/toast";
+import { errorToast, successToast } from "@/components/modules/toast/toast";
 import React from "react";
 
 function SubmitSchedule({ schedules }) {
   const submitHandler = async () => {
     console.log(schedules);
+    let hasConflict = false;
+    schedules.find((schedule) => {
+      const [timeStartH, timeStartM] = schedule.timeStart.split(":");
+      const [timeEndH, timeEndM] = schedule.timeEnd.split(":");
+      console.log(timeStartH, timeEndH);
 
+      const timeStatrMinutes = +timeStartH * 60 + +timeStartM;
+      const timeEndMinutes = +timeEndH * 60 + +timeEndM;
+      if (timeStatrMinutes > timeEndMinutes) {
+        hasConflict = true;
+      }
+      return hasConflict;
+    });
+    if (hasConflict) {
+      return errorToast("زمان شروع یکی از روزها از زمان پایان آن بزرگ تر است");
+    }
     const res = await fetch("/api/schedule", {
       method: "POST",
       headers: {
